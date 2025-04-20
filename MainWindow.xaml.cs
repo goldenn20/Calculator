4bcce2e77293968a2f9e91b9db0460a4e462f499
goldenn <davilmay.cry@yandex.ru>
2025-04-21 01:39:12 +0700

    /

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,23 @@ namespace SimpleCalculator
             InitializeComponent();
         }
 
-        
-        
+        private void DivideClick(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(Input1.Text);
+            double b = double.Parse(Input2.Text);
+
+            if (b == 0)
+            {
+                ResultText.Text = "Делить на 0 нельзя!";
+            }
+            else
+            {
+                double c = a / b;
+                ResultText.Text = "Результат: " + c;
+            }
+        }
+
+
+
     }
 }
