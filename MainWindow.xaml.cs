ab7eb21d495472bdd32e4fe8a777291f3d898dd8
goldenn <davilmay.cry@yandex.ru>
2025-04-21 01:36:52 +0700

    -

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,15 @@ namespace SimpleCalculator
             InitializeComponent();
         }
 
-        
-        
+        private void SubtractClick(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(Input1.Text);
+            double b = double.Parse(Input2.Text);
+            double c = a - b;
+            ResultText.Text = "Результат: " + c;
+        }
+
+
+
     }
 }
